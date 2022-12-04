67ee980189eda9b178e356b6ac10a37f7e05d2f7
jung eunjung <agjjung@avatye.com>
2022-12-05 01:09:11 +0900

    🚚 move : MainActivity 패키지 변경

--- app/src/main/java/com/shootit/greme/ui/view/MainActivity.kt
+++ app/src/main/java/com/shootit/greme/ui/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.shootit.greme
+package com.shootit.greme.ui.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.shootit.greme.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
