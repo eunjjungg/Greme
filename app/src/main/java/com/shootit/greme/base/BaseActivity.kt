c22270fd54470eaaec050cd01fba920cd365692d
jung eunjung <agjjung@avatye.com>
2022-12-06 00:10:28 +0900

    ✨ feature : Base Code 추가
    > Related : #1

--- app/src/main/java/com/shootit/greme/base/BaseActivity.kt
+++ app/src/main/java/com/shootit/greme/base/BaseActivity.kt
@@ -1,19 +1,20 @@
 package com.shootit.greme.base
 
 import android.os.Bundle
+import androidx.annotation.LayoutRes
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
 import androidx.lifecycle.ViewModel
 
-abstract class BaseActivity<VD: ViewDataBinding>(resource: Int) : AppCompatActivity() {
+abstract class BaseActivity<VD: ViewDataBinding>(@LayoutRes val layoutRes: Int) : AppCompatActivity() {
     /*@LayoutRes
     abstract fun getLayoutRes(): Int*/
 
     abstract val viewModel: ViewModel
 
     val binding by lazy {
-        DataBindingUtil.setContentView(this, resource) as VD
+        DataBindingUtil.setContentView(this, layoutRes) as VD
     }
 
     /*val itemDao by lazy {
@@ -21,8 +22,8 @@ abstract class BaseActivity<VD: ViewDataBinding>(resource: Int) : AppCompatActiv
     }*/
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        binding
         super.onCreate(savedInstanceState)
+        binding
         initViewModel(viewModel)
         onCreateAction()
     }
