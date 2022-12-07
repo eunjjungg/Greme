0c8070cd3a909db172a06721c54d7123eeb3f710
jung eunjung <agjjung@avatye.com>
2022-12-07 22:36:33 +0900

    ✨ feature : MainActivity # <타입> : <메세지> 형식으로 작성하며 메세지는 최대 50글자 이내로 무엇을 했는지 명확하게 작성
    ################
    # 본문(추가 설명)
    
    > Related to : #5
    ################
    # 꼬릿말(footer)
    # > Related to : 해당 커밋에 관련된 이슈 번호
    # > Resolved : 이슈 해결했을 때 (= 이슈 닫히는 키워드)
    
    ################
    # 🎉 프로젝트 생성
    # ✨ feature : 새로운 기능 추가
    # ➕ add : 부수적인 코드 추가 및 라이브러리 추가
    # ✏️ update : 내부 코드의 유의미한 수정
    # 🗑️ del : 쓸모없는 코드나 파일 삭제
    # 🚚 move : 프로젝트 내 파일이나 코드의 이동
    # 🐛 fix : 버그 수정
    # 📝 docs : 문서 추가/수정
    # ♻️ refactor : 코드 리팩토링
    # 🚀 deploy : 배포와 관련된 작업
    # 🙈 ignore : .gitignore 파일 수정
    # 🎨 style : 코드 의미에 영향을 주지 않는 변경사항
    # 🤝 merge : 다른 브랜치 MERGE 시 사용
    # ⏪️ revert : 이전 버전으로 되돌리기
    # 🔨 chore : 자잘한 수정사항
    ################
    
    ################
    #   [체크리스트]
    #     제목 끝에 마침표(.) 금지
    #     제목과 본문을 한 줄 띄워 분리하기
    ################

--- app/src/main/java/com/shootit/greme/ui/view/MainActivity.kt
+++ app/src/main/java/com/shootit/greme/ui/view/MainActivity.kt
@@ -1,12 +1,34 @@
 package com.shootit.greme.ui.view
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import com.shootit.greme.R
+import com.shootit.greme.base.BaseActivity
+import com.shootit.greme.databinding.ActivityMainBinding
+import com.shootit.greme.repository.LoginRepository
+import com.shootit.greme.viewmodel.MainViewModel
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
+    override val viewModel by lazy {
+        ViewModelProvider(
+            this, MainViewModel.MainViewModelFactory(LoginRepository.getInstance(application)!!)
+        ).get(MainViewModel::class.java)
     }
+
+    override fun initViewModel(viewModel: ViewModel) {
+        binding.lifecycleOwner = this@MainActivity
+        binding.viewModel = this.viewModel
+    }
+
+    override fun onCreateAction() {
+        binding.btnLogin.setOnClickListener {
+            Intent(this@MainActivity, LoginActivity::class.java)
+                .also { startActivity(it) }
+        }
+    }
+
 }
---
+++ app/src/main/java/com/shootit/greme/viewmodel/MainViewModel.kt
@@ -0,0 +1,27 @@
+package com.shootit.greme.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
+import com.shootit.greme.model.LoginData
+import com.shootit.greme.repository.LoginRepository
+import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
+
+class MainViewModel(private val loginRepository: LoginRepository): ViewModel() {
+
+    fun getLoginData() = viewModelScope.launch {
+        loginRepository.getLoginData()
+    }
+
+
+    class MainViewModelFactory(private val loginRepository: LoginRepository)
+        : ViewModelProvider.Factory {
+        override fun <T : ViewModel> create(modelClass: Class<T>): T {
+            if (modelClass.isAssignableFrom(MainViewModel::class.java)) {
+                return MainViewModel(loginRepository) as T
+            }
+            throw IllegalArgumentException("Not found ViewModel class.")
+        }
+    }
+}
