94090e85efb22410b9ebae57a96206066aa64f7d
jung eunjung <agjjung@avatye.com>
2022-12-18 15:36:33 +0900

    ✨ feature : 관심사 고르는 화면 세팅
    > Related to : #5

--- app/src/main/java/com/shootit/greme/ui/fragment/signup/SetInterestFragment.kt
+++ app/src/main/java/com/shootit/greme/ui/fragment/signup/SetInterestFragment.kt
@@ -6,20 +6,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.activityViewModels
-import androidx.lifecycle.ViewModel
 import com.shootit.greme.R
 import com.shootit.greme.base.BaseFragment
-import com.shootit.greme.databinding.FragmentSetAdditionalInfoBinding
+import com.shootit.greme.databinding.FragmentSetInterestBinding
 import com.shootit.greme.viewmodel.SignUpViewModel
 
-class SetAdditionalInfoFragment
-    : BaseFragment<FragmentSetAdditionalInfoBinding>(
-    R.layout.fragment_set_additional_info
-) {
+class SetInterestFragment : BaseFragment<FragmentSetInterestBinding>(R.layout.fragment_set_interest) {
+
     override val viewModel by activityViewModels<SignUpViewModel>()
 
     override fun initView() {
+        binding.apply {
 
+        }
     }
-
 }
