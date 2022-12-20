ea4e8a3770e23bf8fd35b113a886b66a840ec12f
jung eunjung <agjjung@avatye.com>
2022-12-21 00:42:38 +0900

    ♻️ refactor : 화면 초기화 코드 리팩터링
    > Related to : #5

--- app/src/main/java/com/shootit/greme/ui/fragment/signup/SetInterestFragment.kt
+++ app/src/main/java/com/shootit/greme/ui/fragment/signup/SetInterestFragment.kt
@@ -5,41 +5,49 @@ import com.shootit.greme.R
 import com.shootit.greme.base.BaseFragment
 import com.shootit.greme.databinding.FragmentSetInterestBinding
 import com.shootit.greme.ui.`interface`.InterestButtonClickInterface
+import com.shootit.greme.ui.view.InterestButton
 import com.shootit.greme.viewmodel.SignUpViewModel
 
-class SetInterestFragment : BaseFragment<FragmentSetInterestBinding>(R.layout.fragment_set_interest) {
+class SetInterestFragment :
+    BaseFragment<FragmentSetInterestBinding>(R.layout.fragment_set_interest) {
 
     override val viewModel by activityViewModels<SignUpViewModel>()
 
     override fun initView() {
         binding.viewModel = viewModel
-        binding.initInterestButton(viewModel)
-        binding.apply {
-            setInterestButtonListener()
+        val interestButtons: List<InterestButton> = binding.run {
+            listOf<InterestButton>(
+                btnEnergy, btnUpCycling, btnEcoProduct, btnVeganFood, btnVeganCosmetic
+            )
         }
+
+        setIdView()
+        initInterestButton(interestButtons)
+        binding.setInterestButtonListener(interestButtons)
 
     }
 
-    private fun FragmentSetInterestBinding.setInterestButtonListener() {
+    private fun FragmentSetInterestBinding.setInterestButtonListener(interestButtons: List<InterestButton>) {
         val listener: InterestButtonClickInterface = object : InterestButtonClickInterface {
             override fun interestButtonOnClick(title: String, isClicked: Boolean) {
                 viewModel?.interestSelected(title, isClicked)
             }
         }
 
-        btnEnergy.setButtonListener(listener)
-        btnUpCycling.setButtonListener(listener)
-        btnEcoProduct.setButtonListener(listener)
-        btnVeganFood.setButtonListener(listener)
-        btnVeganCosmetic.setButtonListener(listener)
+        interestButtons.forEach {
+            it.setButtonListener(listener)
+        }
     }
 
-    private fun FragmentSetInterestBinding.initInterestButton(viewModel: SignUpViewModel) {
-        btnEnergy.initButton(viewModel.interestList[SignUpViewModel.INTEREST.ENERGY.index])
-        btnUpCycling.initButton(viewModel.interestList[SignUpViewModel.INTEREST.UP_CYCLING.index])
-        btnEcoProduct.initButton(viewModel.interestList[SignUpViewModel.INTEREST.ECO_PRODUCT.index])
-        btnVeganFood.initButton(viewModel.interestList[SignUpViewModel.INTEREST.VEGAN.index])
-        btnVeganCosmetic.initButton(viewModel.interestList[SignUpViewModel.INTEREST.COSMETIC.index])
+    private fun initInterestButton(interestButtons: List<InterestButton>) {
+        interestButtons.forEachIndexed { index, interestButton ->
+            interestButton.initButton(viewModel.interestList[SignUpViewModel.INTEREST.values()[index].index])
+        }
+    }
+
+    private fun setIdView() {
+        binding.tvWelcome.text =
+            String.format(resources.getString(R.string.signup_interest_welcome), viewModel.id)
     }
 
 }
