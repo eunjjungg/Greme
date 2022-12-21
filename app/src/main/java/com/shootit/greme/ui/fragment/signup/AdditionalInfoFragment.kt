e729e5cecd98d54e24d44513ccc0ebc84da85338
jung eunjung <agjjung@avatye.com>
2022-12-22 00:48:42 +0900

    ✨ feature : dropdown 구현
    > Related to : #5

--- app/src/main/java/com/shootit/greme/ui/fragment/signup/AdditionalInfoFragment.kt
+++ app/src/main/java/com/shootit/greme/ui/fragment/signup/AdditionalInfoFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.ViewModel
 import com.shootit.greme.R
@@ -18,6 +19,14 @@ class AdditionalInfoFragment : BaseFragment<FragmentAdditionalInfoBinding>(R.lay
     override fun initView() {
         binding.vm = viewModel
         binding.apply {  }
+        setRegionDropDown()
+    }
+
+    private fun setRegionDropDown() {
+        val regions = resources.getStringArray(R.array.regionLarge)
+        ArrayAdapter(this.requireContext(), R.layout.dropdown_item, regions)
+            .also { binding.dropdownRegion.setAdapter(it) }
+
     }
 
 }
