455b5cda70829795eeee186fc4fe428847f03796
jung eunjung <agjjung@avatye.com>
2022-12-22 01:26:28 +0900

    ➕ add : 드롭다운 상세 구현
    > Related to : #5

--- app/src/main/java/com/shootit/greme/ui/fragment/signup/AdditionalInfoFragment.kt
+++ app/src/main/java/com/shootit/greme/ui/fragment/signup/AdditionalInfoFragment.kt
@@ -1,32 +1,47 @@
 package com.shootit.greme.ui.fragment.signup
 
-import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
+import android.util.Log
 import android.view.View
-import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.AdapterView.OnItemSelectedListener
 import android.widget.ArrayAdapter
+import android.widget.TextView
 import androidx.fragment.app.activityViewModels
-import androidx.lifecycle.ViewModel
 import com.shootit.greme.R
 import com.shootit.greme.base.BaseFragment
 import com.shootit.greme.databinding.FragmentAdditionalInfoBinding
 import com.shootit.greme.viewmodel.SignUpViewModel
 
-class AdditionalInfoFragment : BaseFragment<FragmentAdditionalInfoBinding>(R.layout.fragment_additional_info) {
+class AdditionalInfoFragment :
+    BaseFragment<FragmentAdditionalInfoBinding>(R.layout.fragment_additional_info) {
     override val viewModel by activityViewModels<SignUpViewModel>()
 
     override fun initView() {
         binding.vm = viewModel
-        binding.apply {  }
-        setRegionDropDown()
+        binding.apply { }
+        initDropDown()
+        setDropDownListener()
     }
 
-    private fun setRegionDropDown() {
+    private fun initDropDown() {
         val regions = resources.getStringArray(R.array.regionLarge)
+        val purpose = resources.getStringArray(R.array.signup_purpose)
         ArrayAdapter(this.requireContext(), R.layout.dropdown_item, regions)
             .also { binding.dropdownRegion.setAdapter(it) }
+        ArrayAdapter(this.requireContext(), R.layout.dropdown_item, purpose)
+            .also { binding.dropdownPurpose.setAdapter(it) }
 
+
+    }
+
+    private fun setDropDownListener() {
+        binding.dropdownPurpose.setOnItemClickListener { adapterView, view, position, rowId ->
+            Log.d("tag", "position: $position, rowId:$rowId, string: ${adapterView.getItemAtPosition(position)}")
+        }
+
+        binding.dropdownRegion.setOnItemClickListener { adapterView, view, position, rowId ->
+            Log.d("tag", "position: $position, rowId:$rowId, string: ${adapterView.getItemAtPosition(position)}")
+        }
     }
 
 }
