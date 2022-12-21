f06b14551e10043ef4b52dd6108e89059b6eaf1e
cyj2825 <cyj2825@naver.com>
2022-12-22 00:12:57 +0900

    ✨ feature : BottomNavigation 구현 완료
    BottomNavigationBar 구현
    
    > Related to : #6

---
+++ app/src/main/java/com/shootit/greme/ui/fragment/SettingFragment.kt
@@ -0,0 +1,23 @@
+package com.shootit.greme.ui.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.shootit.greme.R
+
+class SettingFragment : Fragment(R.layout.fragment_setting) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_setting, container, false)
+    }
+}
