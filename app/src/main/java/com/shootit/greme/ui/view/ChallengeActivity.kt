c9583157593913c643415d936b46707903eda7e7
jung eunjung <agjjung@avatye.com>
2023-01-07 21:39:45 +0900

    ✨ feature : ChallengeActivity 기본 파일 추가

---
+++ app/src/main/java/com/shootit/greme/ui/view/ChallengeActivity.kt
@@ -0,0 +1,12 @@
+package com.shootit.greme.ui.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.shootit.greme.R
+
+class ChallengeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_challenge)
+    }
+}
