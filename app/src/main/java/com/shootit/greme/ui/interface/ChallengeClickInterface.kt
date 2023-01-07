666484522a56574b54437fd7303a076c720a1856
jung eunjung <agjjung@avatye.com>
2023-01-07 20:21:26 +0900

    🔨 chore : rename interface file name

--- app/src/main/java/com/shootit/greme/ui/interface/ChallengeClickInterface.kt
+++ app/src/main/java/com/shootit/greme/ui/interface/ChallengeClickInterface.kt
@@ -3,3 +3,7 @@ package com.shootit.greme.ui.`interface`
 interface ChallengeMenuButtonClickInterface {
     fun challengeMenuOnClick(title: String)
 }
+
+interface ChallengeSummaryClickInterface {
+    fun challengeSummaryOnClick()
+}
