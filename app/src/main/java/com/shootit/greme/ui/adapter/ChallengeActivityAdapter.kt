4e8c5f59f944a064064f25894aeb8430c812c27d
jung eunjung <agjjung@avatye.com>
2023-01-08 13:47:40 +0900

    ✨ feature : 챌린지 화면 구현
    > Related to : #14

---
+++ app/src/main/java/com/shootit/greme/ui/adapter/ChallengeActivityAdapter.kt
@@ -0,0 +1,122 @@
+package com.shootit.greme.ui.adapter
+
+import android.content.res.ColorStateList
+import android.content.res.Resources
+import android.graphics.PorterDuff
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.core.content.res.ResourcesCompat
+import androidx.recyclerview.widget.RecyclerView
+import com.shootit.greme.R
+import com.shootit.greme.databinding.LayoutChallengeDefaultGuideBinding
+import com.shootit.greme.databinding.LayoutChallengeDefaultTopBinding
+import com.shootit.greme.databinding.LayoutChallengeListBinding
+import com.shootit.greme.model.*
+
+class ChallengeActivityAdapter(private val resources: Resources)
+    : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+    var dataList: MutableList<ChallengeRecyclerType> = mutableListOf<ChallengeRecyclerType>()
+    private val TYPE_ONGOING = 0
+    private val TYPE_AVAILABLE = 1
+    private val TYPE_GUIDE = 2
+    private val TYPE_TOPBAR = 3
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+
+        return when (viewType) {
+            TYPE_GUIDE -> {
+                val binding =
+                    LayoutChallengeDefaultGuideBinding.inflate(layoutInflater, parent, false)
+                DefaultGuideViewHolder(binding)
+            }
+            TYPE_TOPBAR -> {
+                val binding =
+                    LayoutChallengeDefaultTopBinding.inflate(layoutInflater, parent, false)
+                DefaultTopViewHolder(binding)
+            }
+            TYPE_ONGOING -> {
+                val binding =
+                    LayoutChallengeListBinding.inflate(layoutInflater, parent, false)
+                OnGoingViewHolder(binding)
+            }
+            // Available().index
+            else -> {
+                val binding =
+                    LayoutChallengeListBinding.inflate(layoutInflater, parent, false)
+                AvailableViewHolder(binding)
+            }
+        }
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        when (holder) {
+            is DefaultGuideViewHolder -> {
+                holder.bind(item = dataList[position] as Guide)
+            }
+            is DefaultTopViewHolder -> {
+                holder.bind(item = dataList[position] as TopBar)
+            }
+            is OnGoingViewHolder -> {
+                holder.bind(item = dataList[position] as OnGoingChallenge)
+            }
+            is AvailableViewHolder -> {
+                holder.bind(item = dataList[position] as AvailableChallenge)
+            }
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return dataList.size
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        return when (dataList[position]) {
+            is OnGoingChallenge -> {
+                TYPE_ONGOING
+            }
+            is Guide -> {
+                TYPE_GUIDE
+            }
+            is AvailableChallenge -> {
+                TYPE_AVAILABLE
+            }
+            is TopBar -> {
+                TYPE_TOPBAR
+            }
+        }
+    }
+
+    inner class DefaultTopViewHolder(private val binding: LayoutChallengeDefaultTopBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(item: TopBar) {
+            Log.d("ccheck", item.successCount.toString())
+        }
+    }
+
+    inner class DefaultGuideViewHolder(private val binding: LayoutChallengeDefaultGuideBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(item: Guide) {
+            Log.d("ccheck", item.text)
+        }
+    }
+
+    inner class OnGoingViewHolder(private val binding: LayoutChallengeListBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(item: OnGoingChallenge) {
+            Log.d("ccheck", item.title)
+            binding.outerView.background = ResourcesCompat.getDrawable(resources, R.drawable.drawble_challenge_list_ongoing, null)
+        }
+    }
+
+    inner class AvailableViewHolder(private val binding: LayoutChallengeListBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(item: AvailableChallenge) {
+            Log.d("ccheck", item.title)
+
+        }
+    }
+}
+
--- app/src/main/java/com/shootit/greme/ui/view/ChallengeActivity.kt
+++ app/src/main/java/com/shootit/greme/ui/view/ChallengeActivity.kt
@@ -2,11 +2,42 @@ package com.shootit.greme.ui.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.lifecycle.ViewModel
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView.LayoutManager
 import com.shootit.greme.R
+import com.shootit.greme.base.BaseActivity
+import com.shootit.greme.databinding.ActivityChallengeBinding
+import com.shootit.greme.model.*
+import com.shootit.greme.ui.adapter.ChallengeActivityAdapter
+import com.shootit.greme.viewmodel.ChallengeViewModel
 
-class ChallengeActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_challenge)
+class ChallengeActivity : BaseActivity<ActivityChallengeBinding>(R.layout.activity_challenge) {
+    override val viewModel by viewModels<ChallengeViewModel> {
+        ChallengeViewModel.ChallengeViewModelFactory("tmp")
     }
+
+    override fun initViewModel(viewModel: ViewModel) {
+        binding.lifecycleOwner = this@ChallengeActivity
+        binding.vm = this.viewModel
+    }
+
+    override fun onCreateAction() {
+        val tmp = mutableListOf<ChallengeRecyclerType>(
+            TopBar(2),
+            Guide("Guide"),
+            OnGoingChallenge("ongoing", "ongoing desc", 3, 3),
+            Guide("Guide"),
+            AvailableChallenge("avail", "avail desc", 10, 10),
+
+            )
+
+        val adapter: ChallengeActivityAdapter = ChallengeActivityAdapter(resources)
+        adapter.dataList = tmp
+        binding.recyclerView.adapter = adapter
+        binding.recyclerView.layoutManager = LinearLayoutManager(this)
+
+    }
+
 }
