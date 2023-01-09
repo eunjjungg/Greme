63279e32d36eb492914d80a29a8aad4c3300421e
cyj2825 <cyj2825@naver.com>
2023-01-09 23:30:57 +0900

    ✨ feature : 다이어리 작성 화면 구현 중
    다이어리 작성 화면 구현 중
    
    > Related to : #15

--- app/src/main/java/com/shootit/greme/ui/fragment/DiaryFragment.kt
+++ app/src/main/java/com/shootit/greme/ui/fragment/DiaryFragment.kt
@@ -1,26 +1,25 @@
 package com.shootit.greme.ui.fragment
 
 import android.os.Bundle
-import android.util.Log
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.TextView
+import androidx.core.widget.addTextChangedListener
 import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.shootit.greme.R
 import com.shootit.greme.databinding.FragmentDiaryBinding
 import com.shootit.greme.model.CalendarData
 import com.shootit.greme.ui.adapter.CalendarAdapter
 import com.jakewharton.threetenabp.AndroidThreeTen
-import org.threeten.bp.DayOfWeek
 import org.threeten.bp.LocalDate
-import org.threeten.bp.Month
 import org.threeten.bp.format.DateTimeFormatter.ofPattern
-import org.threeten.bp.format.TextStyle
-import org.threeten.bp.temporal.TemporalAdjusters.lastDayOfMonth
-import org.threeten.bp.temporal.TemporalAdjusters.previous
+import org.w3c.dom.Text
 import java.util.*
 
 class DiaryFragment : Fragment(R.layout.fragment_diary) {
@@ -41,6 +40,28 @@ class DiaryFragment : Fragment(R.layout.fragment_diary) {
         val root: View = binding.root
 
         AndroidThreeTen.init(context)
+        // edittext 안에 값이 들어갈 경우 background 변경
+        binding.etHashtag.addTextChangedListener(object : TextWatcher{
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                binding.etHashtag.setBackgroundResource(R.drawable.bg_diary_edittext)
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                binding.etHashtag.setBackgroundResource(R.drawable.bg_diary_edittext_write)
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+                binding.etHashtag.setBackgroundResource(R.drawable.bg_diary_edittext_write)
+            }
+        })
+        binding.btnSave.setOnClickListener {
+            binding.clToday.visibility = View.VISIBLE
+            binding.clDisclosure.visibility = View.GONE
+            binding.btnSave.visibility = View.GONE
+            binding.clEdit.visibility = View.VISIBLE
+        }
+        setupSpinner()
+        setupSpinnerHandler()
         return root
     }
 
@@ -56,8 +77,8 @@ class DiaryFragment : Fragment(R.layout.fragment_diary) {
             // 오늘 요일 출력
             val forDay = LocalDate.now().dayOfWeek
             var weekday = ""
-            if(forDay.toString() == "SATURDAY"){
-                weekday = "SAT"
+            if(forDay.toString() == "SUNDAY"){
+                weekday = "SUN"
             }
             val now = LocalDate.now().format(ofPattern("d"))
             binding.tvMonth.text = LocalDate.now().month.toString()
@@ -68,14 +89,34 @@ class DiaryFragment : Fragment(R.layout.fragment_diary) {
 
                 calendarList.apply {
                     // 오늘을 기준으로 +-3일 값들 출력
-                    // todo : week_day 값만 변경하자!!!
                     add(CalendarData((now.toInt()+i.toLong()-3).toString(),weekday))
                 }
-                // Log.d("저번 주 일요일 기준으로 시작!", preSunday.plusDays(i.toLong()).format(dateFormat))
             }
         }
         binding.rvCalendar.adapter = calendarAdapter
         binding.rvCalendar.layoutManager = GridLayoutManager(context, 7)
+    }
+    private fun setupSpinner() {
+        val challenge = resources.getStringArray(R.array.spinner_challenge)
+        val adapter = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, challenge)
+        adapter.setDropDownViewResource(R.layout.spinner_style)
+        binding.spinnerDiary.adapter = adapter
+    }
+    private fun setupSpinnerHandler() {
+        binding.spinnerDiary.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+
+            }
+
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+
+            }
+        }
     }
     override fun onDestroyView() {
         super.onDestroyView()
