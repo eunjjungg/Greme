e0443705a895b75c513b3392f97aa637b64141a2
jung eunjung <agjjung@avatye.com>
2023-01-10 01:56:14 +0900

    ✨ feature : programmatically movable view 구현
    > Related to : #5

--- app/src/main/java/com/shootit/greme/ui/adapter/ChallengeInfoRecyclerAdapter.kt
+++ app/src/main/java/com/shootit/greme/ui/adapter/ChallengeInfoRecyclerAdapter.kt
@@ -1,23 +1,29 @@
 package com.shootit.greme.ui.adapter
 
 import android.content.res.Resources
+import android.util.Log
+import android.util.Size
+import android.util.TypedValue
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.ViewTreeObserver.OnGlobalLayoutListener
+import android.widget.LinearLayout
 import androidx.core.content.res.ResourcesCompat
 import androidx.core.view.ViewCompat
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.internal.ViewUtils.addOnGlobalLayoutListener
 import com.shootit.greme.R
-import com.shootit.greme.databinding.ActivityChallengeInfoBinding
 import com.shootit.greme.databinding.LayoutChallengeDefaultGuideBinding
+import com.shootit.greme.databinding.LayoutChallengeGridImageBinding
 import com.shootit.greme.databinding.LayoutChallengeListBinding
 import com.shootit.greme.model.*
+
 
 class ChallengeInfoRecyclerAdapter(private val resources: Resources)
     : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
     var imgDataList: MutableList<ChallengeInfoImg> = mutableListOf()
     var challengeInfo: ChallengeInfo = ChallengeInfo("null", "null", 0, false)
-    private val challengeInfoGuide = ChallengeInfoGuide()
 
     private val TYPE_INFO = 0
     private val TYPE_GUIDE = 1
@@ -36,7 +42,7 @@ class ChallengeInfoRecyclerAdapter(private val resources: Resources)
                 DefaultGuideViewHolder(binding)
             }
             else -> {
-                val binding = LayoutChallengeListBinding.inflate(layoutInflater, parent, false)
+                val binding = LayoutChallengeGridImageBinding.inflate(layoutInflater, parent, false)
                 ImageViewHolder(binding)
             }
         }
@@ -51,13 +57,13 @@ class ChallengeInfoRecyclerAdapter(private val resources: Resources)
                 holder.bind(challengeInfo)
             }
             is ImageViewHolder -> {
-                holder.bind(imgDataList[position - 2])
+                holder.bind(imgDataList[position - TYPE_IMG])
             }
         }
     }
 
     override fun getItemCount(): Int {
-        return imgDataList.size + 2
+        return imgDataList.size + TYPE_IMG
     }
 
     override fun getItemViewType(position: Int): Int {
@@ -67,7 +73,7 @@ class ChallengeInfoRecyclerAdapter(private val resources: Resources)
     private inner class DefaultGuideViewHolder(private val binding: LayoutChallengeDefaultGuideBinding)
         : RecyclerView.ViewHolder(binding.root) {
         fun bind() {
-            binding.tv.text = "챌린지 참여 목록"
+            binding.tv.text = "챌린지 참여 목록\n"
         }
     }
 
@@ -86,9 +92,37 @@ class ChallengeInfoRecyclerAdapter(private val resources: Resources)
         }
     }
 
-    private inner class ImageViewHolder(private val binding: LayoutChallengeListBinding) : RecyclerView.ViewHolder(binding.root) {
+    private inner class ImageViewHolder(private val binding: LayoutChallengeGridImageBinding) : RecyclerView.ViewHolder(binding.root) {
         fun bind(challengeInfoImg: ChallengeInfoImg) {
+            val outerLayout = binding.outerView
+            val outerParams: ViewGroup.LayoutParams = outerLayout.layoutParams
+            val innerLayoutList = listOf(binding.imgView1, binding.imgView2, binding.imgView3)
+            val innerParamsList = listOf(binding.imgView1.layoutParams, binding.imgView2.layoutParams, binding.imgView3.layoutParams)
 
+            outerLayout.viewTreeObserver.addOnGlobalLayoutListener(
+                object : OnGlobalLayoutListener {
+                    override fun onGlobalLayout() {
+                        outerLayout.getViewTreeObserver().removeGlobalOnLayoutListener(this)
+                        val width: Int = outerLayout.measuredWidth
+                        outerParams.height = width / 3
+                        outerLayout.layoutParams = outerParams
+                        innerParamsList.forEachIndexed { index, params ->
+                            params.height = width / 3
+                            params.width = width / 3
+                            innerLayoutList[index].layoutParams = params
+
+                        }
+
+                    }
+                })
+
+
+            if(challengeInfoImg.secondImgUrl == null){
+                binding.imgView2.setImageDrawable(ResourcesCompat.getDrawable(resources,R.drawable.drawable_challenge_summary, null))
+            }
+            if(challengeInfoImg.thirdImgUrl == null) {
+                binding.imgView3.setImageDrawable(ResourcesCompat.getDrawable(resources,R.drawable.drawable_challenge_summary, null))
+            }
         }
     }
 }
