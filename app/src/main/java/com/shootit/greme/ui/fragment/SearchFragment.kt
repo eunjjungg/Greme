a1886d657825a3f9e70020afe4774bb3d2b6ca17
cyj2825 <cyj2825@naver.com>
2023-01-13 22:55:08 +0900

    ✨ feature : 찾기 화면 구현 완료
    찾기 화면 구현 완료
    
    > Related to : #18

--- app/src/main/java/com/shootit/greme/ui/fragment/SearchFragment.kt
+++ app/src/main/java/com/shootit/greme/ui/fragment/SearchFragment.kt
@@ -5,56 +5,63 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.widget.SearchView
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.shootit.greme.R
+import com.shootit.greme.databinding.FragmentSearchBinding
+import com.shootit.greme.model.SearchData
+import com.shootit.greme.ui.adapter.SearchAdapter
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [SearchFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class SearchFragment : Fragment(R.layout.fragment_search) {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    // 전역 변수로 바인딩 객체 선언
+    private var mBinding: FragmentSearchBinding? = null
+    // 매번 null 체크를 할 필요없이 편의성을 위해 바인딩 변수 재선언
+    private val binding get() = mBinding!!
+
+    private lateinit var rvSearch: RecyclerView
+    private lateinit var searchView: SearchView
+    private var mList = ArrayList<SearchData>()
+    private lateinit var adapter: SearchAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search, container, false)
+        mBinding = FragmentSearchBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        rvSearch = binding.rvSearch
+        searchView = binding.searchView
+
+        rvSearch.setHasFixedSize(true)
+        searchView.isSubmitButtonEnabled = true
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener{
+            override fun onQueryTextChange(newText: String?): Boolean {
+                return false
+            }
+
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                rvSearch.layoutManager = GridLayoutManager(context, 3)
+                addDataToList()
+                adapter = SearchAdapter(mList)
+                rvSearch.adapter = adapter
+                return false
+            }
+        })
+
+
+        return root
+    }
+    private fun addDataToList(){
+        mList.add(SearchData(R.drawable.ex_img))
+        mList.add(SearchData(R.drawable.ex_img))
+        mList.add(SearchData(R.drawable.ex_img2))
+        mList.add(SearchData(R.drawable.ex_img2))
+        mList.add(SearchData(R.drawable.ex_img))
+        mList.add(SearchData(R.drawable.ex_img2))
+
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment SearchFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            SearchFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
 }
