5f25ce4d23cc32347d99771e7100adb739da1363
jung eunjung <agjjung@avatye.com>
2023-01-14 22:08:49 +0900

    ✨ feature : challenge progress circular bar 구현
    > Related to : #14

--- app/src/main/java/com/shootit/greme/ui/view/ChallengeActivity.kt
+++ app/src/main/java/com/shootit/greme/ui/view/ChallengeActivity.kt
@@ -2,21 +2,28 @@ package com.shootit.greme.ui.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import androidx.activity.viewModels
 import androidx.core.view.ViewCompat
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModel
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView.LayoutManager
+import com.google.android.material.snackbar.Snackbar
 import com.shootit.greme.R
 import com.shootit.greme.base.BaseActivity
 import com.shootit.greme.databinding.ActivityChallengeBinding
 import com.shootit.greme.model.*
+import com.shootit.greme.repository.ChallengeRepository
 import com.shootit.greme.ui.adapter.ChallengeActivityAdapter
 import com.shootit.greme.viewmodel.ChallengeViewModel
 
 class ChallengeActivity : BaseActivity<ActivityChallengeBinding>(R.layout.activity_challenge) {
     override val viewModel by viewModels<ChallengeViewModel> {
-        ChallengeViewModel.ChallengeViewModelFactory("tmp")
+        ChallengeViewModel.ChallengeViewModelFactory(
+            ChallengeRepository.getInstance()!!
+        )
     }
 
     override fun initViewModel(viewModel: ViewModel) {
@@ -24,24 +31,53 @@ class ChallengeActivity : BaseActivity<ActivityChallengeBinding>(R.layout.activi
         binding.vm = this.viewModel
     }
 
+    private val adapter by lazy {
+        ChallengeActivityAdapter(resources)
+    }
+
     override fun onCreateAction() {
         ViewCompat.setTransitionName(binding.icon, "icon")
-
-        val tmp = mutableListOf<ChallengeRecyclerType>(
-            TopBar(2),
-            Guide("참여중인 챌린지"),
-            OnGoingChallenge("#가까운_거리는_걸어다니기", "도보로 15분 이내의 거리는 걸어다니기", 3, 23),
-            OnGoingChallenge("#텀블러_이용하기", "카페가서 텀블러 이용하기", 12, 25),
-            Guide("참여 가능 챌린지"),
-            AvailableChallenge("#따뜻하게_입고_다니기", "난방 대신 따뜻한 옷을 입어요", 10, 13),
-
-            )
-
-        val adapter: ChallengeActivityAdapter = ChallengeActivityAdapter(resources)
-        adapter.dataList = tmp
         binding.recyclerView.adapter = adapter
         binding.recyclerView.layoutManager = LinearLayoutManager(this)
+        setObserver()
 
     }
 
+    override fun onResume() {
+        super.onResume()
+        binding.recyclerView.visibility = View.GONE
+        viewModel.getMyChallengeList {
+            val serverData: ChallengeActivityModel
+
+            if (it == null) {
+                binding.recyclerView.visibility = View.GONE
+                binding.progressBar.visibility = View.GONE
+                makeSnackBar("접속이 불안정합니다. 잠시 후 다시 시도해주세요.")
+                return@getMyChallengeList
+            } else {
+                serverData = it
+                binding.recyclerView.visibility = View.VISIBLE
+                binding.progressBar.visibility = View.GONE
+            }
+
+            // TODO 데이터 세팅
+            Log.d("ccheck challenge", it.toString())
+        }
+    }
+
+    private fun setObserver() {
+        viewModel.challengeData.observe(this, Observer {
+            val changeRange = adapter.dataList.size - 2
+            adapter.dataList = it
+            adapter.notifyItemRangeChanged(2, changeRange)
+        })
+    }
+
+    private fun makeSnackBar(message: String) {
+        val snackbar = Snackbar.make(binding.recyclerView.parent as View, message, Snackbar.LENGTH_LONG)
+        snackbar.setAction("확인", View.OnClickListener {
+            snackbar.dismiss()
+        })
+        snackbar.show()
+    }
 }
