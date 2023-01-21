80fe50cce136f2aae5f16f0ec93a06f234b95242
jung eunjung <agjjung@avatye.com>
2023-01-22 05:54:19 +0900

    ✨ feature : Setting User Info 구현
    > Related to : #26

--- app/src/main/java/com/shootit/greme/ui/view/SettingUserInfoActivity.kt
+++ app/src/main/java/com/shootit/greme/ui/view/SettingUserInfoActivity.kt
@@ -1,26 +1,30 @@
 package com.shootit.greme.ui.view
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.ArrayAdapter
 import androidx.activity.viewModels
-import androidx.core.view.ViewCompat
 import androidx.lifecycle.ViewModel
+import com.google.android.material.snackbar.Snackbar
 import com.shootit.greme.R
 import com.shootit.greme.base.BaseActivity
 import com.shootit.greme.databinding.ActivitySettingUserInfoBinding
-import com.shootit.greme.repository.ChallengeRepository
-import com.shootit.greme.viewmodel.ChallengeInfoViewModel
+import com.shootit.greme.model.GENDER
+import com.shootit.greme.repository.SettingRepository
+import com.shootit.greme.ui.`interface`.InterestButtonClickInterface
+import com.shootit.greme.ui.custom.InterestButton
 import com.shootit.greme.viewmodel.SettingUserInfoViewModel
+import com.shootit.greme.viewmodel.SignUpViewModel
 
 /**
  * 넘겨받는 데이터 : none
  */
 
-class SettingUserInfoActivity : BaseActivity<ActivitySettingUserInfoBinding>(R.layout.activity_setting_user_info) {
+class SettingUserInfoActivity :
+    BaseActivity<ActivitySettingUserInfoBinding>(R.layout.activity_setting_user_info) {
     override val viewModel by viewModels<SettingUserInfoViewModel> {
         SettingUserInfoViewModel.SettingUserInfoViewModelFactory(
-            "tmp"
+            SettingRepository.getInstance()!!
         )
     }
 
@@ -31,7 +35,162 @@ class SettingUserInfoActivity : BaseActivity<ActivitySettingUserInfoBinding>(R.l
 
     override fun onCreateAction() {
         //ViewCompat.setTransitionName(binding.tvPageName, "pageName")
-
+        initData()
+        initDropDownAdapter()
+        setInterestButtonListener()
+        setGenderButtonListener(listOf(binding.btnMale, binding.btnFemale, binding.btnWhatever))
+        setPurposeDropDownListener()
+        setRegionDropDownListener()
+        setNextButtonListener()
     }
 
+    private fun initData() {
+        viewModel.getInitData(
+            initInterest = { initInterestButton(it) },
+            initGender = { initGenderButton(it) },
+            initArea = { initRegionDropDown(it) },
+            initPurpose = { initPurposeDropDown(it) },
+            errorCase = { makeSnackBar("접속이 불안정합니다. 잠시 후 다시 시도해주세요.") }
+        )
+    }
+
+    private fun initInterestButton(interestList: List<SignUpViewModel.INTEREST>) {
+        val interestButtons = listOf<InterestButton>(
+            binding.btnEnergy,
+            binding.btnUpCycling,
+            binding.btnEcoProduct,
+            binding.btnVeganFood,
+            binding.btnVeganCosmetic
+        )
+        interestList.forEach { interest ->
+            interestButtons[interest.index].initButton(true)
+        }
+    }
+
+    private fun initGenderButton(gender: GENDER) {
+        when (gender) {
+            GENDER.Male -> binding.btnMale.initButton(true)
+            GENDER.Female -> binding.btnFemale.initButton(true)
+            GENDER.Whatever -> binding.btnWhatever.initButton(true)
+        }
+    }
+
+    private fun initDropDownAdapter() {
+        val regions = resources.getStringArray(R.array.regionLarge)
+        val purpose = resources.getStringArray(R.array.signup_purpose)
+        ArrayAdapter(binding.root.context, R.layout.dropdown_item, regions)
+            .also { binding.dropdownRegion.setAdapter(it) }
+        ArrayAdapter(binding.root.context, R.layout.dropdown_item, purpose)
+            .also { binding.dropdownPurpose.setAdapter(it) }
+    }
+
+    private fun initPurposeDropDown(purpose: String) {
+        binding.dropdownPurpose.setText(purpose, false)
+    }
+
+    private fun initRegionDropDown(region: String) {
+        binding.dropdownRegion.setText(region, false)
+    }
+
+    private fun setInterestButtonListener() {
+        val interestButtons = listOf<InterestButton>(
+            binding.btnEnergy,
+            binding.btnUpCycling,
+            binding.btnEcoProduct,
+            binding.btnVeganFood,
+            binding.btnVeganCosmetic
+        )
+
+        val listener: InterestButtonClickInterface = object : InterestButtonClickInterface {
+            override fun interestButtonOnClick(title: String, isClicked: Boolean) {
+                viewModel.interestSelected(title, isClicked)
+            }
+        }
+
+        interestButtons.forEach {
+            it.setButtonListener(listener)
+        }
+    }
+
+    private fun setGenderButtonListener(genderButtons: List<InterestButton>) {
+        val listener: InterestButtonClickInterface = object : InterestButtonClickInterface {
+            override fun interestButtonOnClick(title: String, isClicked: Boolean) {
+                val initButton: (List<InterestButton>) -> Unit = {
+                    it.forEach { button -> button.initButton(false) }
+                }
+                if (isClicked) {
+                    when (title) {
+                        GENDER.Male.text -> {
+                            initButton(listOf(binding.btnFemale, binding.btnWhatever))
+                            viewModel.gender = GENDER.Male
+                        }
+                        GENDER.Female.text -> {
+                            initButton(listOf(binding.btnMale, binding.btnWhatever))
+                            viewModel.gender = GENDER.Female
+                        }
+                        else -> {
+                            initButton(listOf(binding.btnMale, binding.btnFemale))
+                            viewModel.gender = GENDER.Whatever
+                        }
+                    }
+                } else {
+                    val setNull: (GENDER) -> Unit = {
+                        if (viewModel.gender == it)
+                            viewModel.gender = null
+                    }
+                    when (title) {
+                        GENDER.Male.text -> {
+                            setNull(GENDER.Male)
+                        }
+                        GENDER.Female.text -> {
+                            setNull(GENDER.Female)
+                        }
+                        else -> {
+                            setNull(GENDER.Whatever)
+                        }
+                    }
+                }
+            }
+        }
+
+        genderButtons.forEach {
+            it.setButtonListener(listener)
+        }
+    }
+
+
+    private fun setPurposeDropDownListener() {
+        binding.dropdownPurpose.setOnItemClickListener { adapterView, view, position, rowId ->
+            viewModel.purpose = adapterView.getItemAtPosition(position).toString()
+        }
+    }
+
+    private fun setRegionDropDownListener() {
+        binding.dropdownRegion.setOnItemClickListener { adapterView, view, position, rowId ->
+            viewModel.region = adapterView.getItemAtPosition(position).toString()
+        }
+    }
+
+    private fun setNextButtonListener() {
+        binding.btnNext.setOnClickListener {
+            viewModel.saveUserInfo(
+                { makeSnackBar(it) },
+                {
+                    if (it) {
+                        makeSnackBar("프로필 업데이트 완료!")
+                    } else {
+                        makeSnackBar("저장에 실패하였습니다. 다시 시도해주세요.")
+                    }
+                }
+            )
+        }
+    }
+
+    private fun makeSnackBar(message: String) {
+        val snackbar = Snackbar.make(binding.view, message, Snackbar.LENGTH_LONG)
+        snackbar.setAction("확인", View.OnClickListener {
+            snackbar.dismiss()
+        })
+        snackbar.show()
+    }
 }
--- app/src/main/java/com/shootit/greme/viewmodel/SettingUserInfoViewModel.kt
+++ app/src/main/java/com/shootit/greme/viewmodel/SettingUserInfoViewModel.kt
@@ -2,14 +2,92 @@ package com.shootit.greme.viewmodel
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
+import com.shootit.greme.model.GENDER
+import com.shootit.greme.model.UserAreaAndPurposeInfo
+import com.shootit.greme.model.UserInterestAndGenderInfo
+import com.shootit.greme.repository.SettingRepository
+import com.shootit.greme.util.EnumUtil.getGenderByInt
+import com.shootit.greme.util.EnumUtil.getInterestByInt
+import kotlinx.coroutines.launch
 
-class SettingUserInfoViewModel(private val tmp: String): ViewModel() {
+class SettingUserInfoViewModel(private val settingRepository: SettingRepository): ViewModel() {
 
-    class SettingUserInfoViewModelFactory(private val tmp: String)
+    var gender: GENDER? = GENDER.Whatever
+    var region: String = "선택 안 함"
+    var purpose: String = "선택 안 함"
+    val interestList = mutableListOf<SignUpViewModel.INTEREST>()
+
+    fun getInitData(
+        initInterest: (List<SignUpViewModel.INTEREST>) -> Unit,
+        initGender: (GENDER) -> Unit,
+        initArea: (String) -> Unit,
+        initPurpose: (String) -> Unit,
+        errorCase: () -> Unit
+    ) = viewModelScope.launch {
+        val data = settingRepository.getCurrentProfile()
+        data?.let {
+            it.interestType.forEach { interestInt ->
+                interestList.add(interestInt.getInterestByInt())
+            }
+            gender = it.genderType.getGenderByInt()
+            region = it.area
+            purpose = it.purpose
+
+            initInterest(interestList)
+            initGender(it.genderType.getGenderByInt())
+            initArea(it.area)
+            initPurpose(it.purpose)
+        }
+        if (data == null) {
+            errorCase()
+        }
+    }
+
+    fun saveUserInfo(notAnswered: (String) -> Unit, completion: (Boolean) -> Unit) {
+        viewModelScope.launch {
+            if (gender == null || interestList.size == 0) {
+                notAnswered("모든 항목을 선택해주세요")
+                return@launch
+            }
+
+            val response1 = settingRepository.setUserAreaAndPurpose(
+                UserAreaAndPurposeInfo(area = region, purpose = purpose)
+            )
+            val interestIndexList = mutableListOf<Int>()
+            interestList.forEach { interest ->
+                interestIndexList.add(interest.index)
+            }
+            val response2 = settingRepository.setUserInterestAndGender(
+                UserInterestAndGenderInfo(interestIndexList, gender!!.index)
+            )
+
+            completion(response1 && response2)
+        }
+    }
+
+    fun interestSelected(title: String, isClicked: Boolean) {
+        val settingList : (SignUpViewModel.INTEREST) -> Unit = { interest ->
+            if(isClicked) {
+                interestList.add(interest)
+            } else {
+                interestList.remove(interest)
+            }
+        }
+        when (title) {
+            SignUpViewModel.INTEREST.ENERGY.text -> settingList(SignUpViewModel.INTEREST.ENERGY)
+            SignUpViewModel.INTEREST.UP_CYCLING.text -> settingList(SignUpViewModel.INTEREST.UP_CYCLING)
+            SignUpViewModel.INTEREST.ECO_PRODUCT.text -> settingList(SignUpViewModel.INTEREST.ECO_PRODUCT)
+            SignUpViewModel.INTEREST.VEGAN.text -> settingList(SignUpViewModel.INTEREST.VEGAN)
+            SignUpViewModel.INTEREST.COSMETIC.text -> settingList(SignUpViewModel.INTEREST.COSMETIC)
+        }
+    }
+
+    class SettingUserInfoViewModelFactory(private val settingRepository: SettingRepository)
         : ViewModelProvider.Factory {
         override fun <T : ViewModel> create(modelClass: Class<T>): T {
             if (modelClass.isAssignableFrom(SettingUserInfoViewModel::class.java)) {
-                return SettingUserInfoViewModel(tmp) as T
+                return SettingUserInfoViewModel(settingRepository) as T
             }
             throw IllegalArgumentException("Not found ViewModel class.")
         }
