63d18574994e86946754fb688f514b1c2f330c0f
cyj2825 <cyj2825@naver.com>
2023-01-21 23:45:33 +0900

    ✨ feature : 삭제하기 다이얼로그 창 띄우기 완료
    삭제하기 다이얼로그 창 띄우기 완료
    
    > Related to : #22

--- app/src/main/java/com/shootit/greme/ui/fragment/DiaryFragment.kt
+++ app/src/main/java/com/shootit/greme/ui/fragment/DiaryFragment.kt
@@ -2,8 +2,11 @@ package com.shootit.greme.ui.fragment
 
 import android.Manifest
 import android.app.Activity.RESULT_OK
+import android.app.AlertDialog
+import android.content.DialogInterface
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.graphics.Color
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
@@ -61,6 +64,57 @@ class DiaryFragment : Fragment(R.layout.fragment_diary) {
 
     lateinit var calendarAdapter: CalendarAdapter
     private var calendarList = ArrayList<CalendarData>()
+
+    val positiveButtonClick = { dialogInterface: DialogInterface, i: Int ->
+        // 삭제하기 버튼 서버 연동
+        Log.d("TestLog", "Diary")
+        // 서버로 보낼 로그인 데이터 생성
+        val diaryDeleteData = DiaryDeleteData(postId.toInt())
+        Log.d("datavalue", "data값=> "+ diaryDeleteData)
+        Log.d("datavalue", "postId Int값=> "+ postId.toInt())
+        // 현재 사용자의 정보를 받아올 것을 명시
+        // 서버 통신은 I/O 작업이므로 비동기적으로 받아올 Callback 내부 코드는 나중에 데이터를 받아오고 실행
+        val call: Call<Void> = ConnectionObject.getDiaryWriteRetrofitService.diaryDelete(diaryDeleteData)
+        call.enqueue(object : Callback<Void>{
+            override fun onResponse(call: Call<Void>,response: Response<Void>) {
+                val data = response.code()
+                Log.d("status code", data.toString())
+                // 네트워크 통신에 성공한 경우
+                if(response.isSuccessful){
+                    // 다이어리 작성한 것들 초기화하기
+                    binding.ivMain.setImageResource(R.drawable.ic_plus)
+                    binding.etHashtag.text = null
+                    binding.etHashtag.setBackgroundResource(R.drawable.bg_diary_edittext)
+                    binding.etContent.text = null
+                    binding.etContent.setBackgroundResource(R.drawable.bg_diary_edittext)
+                    // 저장하기 버튼 다시 생성
+                    binding.clToday.visibility = View.GONE
+                    binding.clDisclosure.visibility = View.VISIBLE
+                    binding.btnSave.visibility = View.VISIBLE
+                    binding.clEdit.visibility = View.GONE
+
+                    Log.d("Network_Delete", "success")
+                    val data = response.body().toString()
+                    Log.d("responsevalue", "response값=> "+ data)
+                } else
+                {
+                    // 이곳은 에러 발생할 경우 실행됨
+                    val data = response.code()
+                    Log.d("status code", data.toString())
+                    val data2 = response.headers()
+                    Log.d("header", data2.toString())
+                    Log.d("server err", response.errorBody()?.string().toString())
+                    Log.d("Network_Delete", "fail")
+                }
+            }
+            override fun onFailure(call: Call<Void>, t: Throwable) {
+                Log.d("Network_Delete", "error!")
+            }
+        })
+    }
+    val negativeButtonClick = { dialogInterface: DialogInterface, i: Int ->
+        toast("취소")
+    }
 
     companion object {
         // 갤러리 권한 요청
@@ -136,7 +190,6 @@ class DiaryFragment : Fragment(R.layout.fragment_diary) {
                 .commitNow()
         }
 
-        /*
         // 다이어리 전체 보기 서버 연동
         binding.ivCalendar.setOnClickListener {
             Log.d("Network_Entire", "entireDiary")
@@ -147,8 +200,14 @@ class DiaryFragment : Fragment(R.layout.fragment_diary) {
                     response: Response<List<ResponseEntireDiaryData>>
                 ) {
                     if (response.isSuccessful){
-                        val data = response.body().toString()
-                        Log.d("responsevalue", "entireDiary_response 값 => "+ data)
+                        // val data = response.body().toString()
+                        val itemdata1 = response.body()?.get(0)
+                        val itemdata2 = response.body()?.get(1)
+                        val itemdata3 = response.body()?.get(2)
+
+                        Log.d("responsevalue", "itemdata1_response 값 => "+ itemdata1)
+                        Log.d("responsevalue", "itemdata2_response 값 => "+ itemdata2)
+                        Log.d("responsevalue", "itemdata3_response 값 => "+ itemdata3)
                     }else{
                         // 이곳은 에러 발생할 경우 실행됨
 
@@ -161,8 +220,9 @@ class DiaryFragment : Fragment(R.layout.fragment_diary) {
 
                 }
             })
-        }*/
+        }
 
+        /*
         // 날짜로 다이어리 조회 서버 연동
         binding.ivCalendar.setOnClickListener {
             Log.d("Network_Date", "dateDiary")
@@ -191,54 +251,26 @@ class DiaryFragment : Fragment(R.layout.fragment_diary) {
 
                 }
             })
-        }
+        }*/
 
-        // 삭제하기 버튼 서버 연동
+        // 삭제하기 버튼 다이얼로그 창 띄우기
         binding.btnDelete.setOnClickListener {
-            Log.d("TestLog", "Diary")
-            // 서버로 보낼 로그인 데이터 생성
-            val diaryDeleteData = DiaryDeleteData(postId.toInt())
-            Log.d("datavalue", "data값=> "+ diaryDeleteData)
-            Log.d("datavalue", "postId Int값=> "+ postId.toInt())
-            // 현재 사용자의 정보를 받아올 것을 명시
-            // 서버 통신은 I/O 작업이므로 비동기적으로 받아올 Callback 내부 코드는 나중에 데이터를 받아오고 실행
-            val call: Call<Void> = ConnectionObject.getDiaryWriteRetrofitService.diaryDelete(diaryDeleteData)
-            call.enqueue(object : Callback<Void>{
-                override fun onResponse(call: Call<Void>,response: Response<Void>) {
-                    val data = response.code()
-                    Log.d("status code", data.toString())
-                    // 네트워크 통신에 성공한 경우
-                    if(response.isSuccessful){
-                        // 다이어리 작성한 것들 초기화하기
-                        binding.ivMain.setImageResource(R.drawable.ic_plus)
-                        binding.etHashtag.text = null
-                        binding.etHashtag.setBackgroundResource(R.drawable.bg_diary_edittext)
-                        binding.etContent.text = null
-                        binding.etContent.setBackgroundResource(R.drawable.bg_diary_edittext)
-                        // 저장하기 버튼 다시 생성
-                        binding.clToday.visibility = View.GONE
-                        binding.clDisclosure.visibility = View.VISIBLE
-                        binding.btnSave.visibility = View.VISIBLE
-                        binding.clEdit.visibility = View.GONE
+            val builder = AlertDialog.Builder(requireContext())
+            builder.setTitle("정말 삭제하시겠습니까?")
+                .setMessage("해당 다이어리와 게시물이 모두 삭제됩니다.")
+                .setPositiveButton("확인",positiveButtonClick)
+                .setNegativeButton("취소", negativeButtonClick)
 
-                        Log.d("Network_Delete", "success")
-                        val data = response.body().toString()
-                        Log.d("responsevalue", "response값=> "+ data)
-                    } else
-                    {
-                        // 이곳은 에러 발생할 경우 실행됨
-                        val data = response.code()
-                        Log.d("status code", data.toString())
-                        val data2 = response.headers()
-                        Log.d("header", data2.toString())
-                        Log.d("server err", response.errorBody()?.string().toString())
-                        Log.d("Network_Delete", "fail")
-                    }
-                }
-                override fun onFailure(call: Call<Void>, t: Throwable) {
-                    Log.d("Network_Delete", "error!")
-                }
-            })
+            val alertDialog = builder.create()
+            alertDialog.show()
+            val button1 = alertDialog.getButton(DialogInterface.BUTTON_POSITIVE)
+            with(button1){
+                setTextColor(Color.RED)
+            }
+            val button2 = alertDialog.getButton(DialogInterface.BUTTON_NEGATIVE)
+            with(button2){
+                setTextColor(Color.BLUE)
+            }
         }
         setupSpinner()
         setupSpinnerHandler()
@@ -284,13 +316,19 @@ class DiaryFragment : Fragment(R.layout.fragment_diary) {
             imageFile = File(getRealPathFromURI(imageWideUri!!))
             // 서버로 보내기 위해 RequestBody객체로 변환
             val requestFile = RequestBody.create("image/*".toMediaTypeOrNull(), imageFile!!)
-            val body = MultipartBody.Part.createFormData("multipartFile", imageFile!!.name, requestFile)
-            Log.d("datavalue", "multipart값=> " + body)
+            val body =
+                MultipartBody.Part.createFormData("multipartFile", imageFile!!.name, requestFile)
 
             // 현재 사용자의 정보를 받아올 것을 명시
             // 서버 통신은 I/O 작업이므로 비동기적으로 받아올 Callback 내부 코드는 나중에 데이터를 받아오고 실행
             val call: Call<Long> =
-                ConnectionObject.getDiaryWriteRetrofitService.diaryWrite(binding.etContent.text.toString(), binding.etHashtag.text.toString(), 1, binding.cbDisclosure.isChecked, body)
+                ConnectionObject.getDiaryWriteRetrofitService.diaryWrite(
+                    binding.etContent.text.toString(),
+                    binding.etHashtag.text.toString(),
+                    1,
+                    binding.cbDisclosure.isChecked,
+                    body
+                )
             call.enqueue(object : Callback<Long> {
                 override fun onResponse(
                     call: Call<Long>, response: Response<Long>
@@ -324,68 +362,67 @@ class DiaryFragment : Fragment(R.layout.fragment_diary) {
                 }
             })
         }
-
-
-}
-
-private fun setupSpinner() {
-val adapter = ArrayAdapter.createFromResource(requireContext(), R.array.spinner_challenge,R.layout.color_spinner_layout)
-adapter.setDropDownViewResource(R.layout.spinner_style)
-binding.spinnerDiary.adapter = adapter
-}
-private fun setupSpinnerHandler() {
-binding.spinnerDiary.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
-    override fun onItemSelected(
-        parent: AdapterView<*>?,
-        view: View?,
-        position: Int,
-        id: Long
-    ) {
-
     }
-
-    override fun onNothingSelected(p0: AdapterView<*>?) {
-
+    private fun setupSpinner() {
+        val adapter = ArrayAdapter.createFromResource(requireContext(), R.array.spinner_challenge,R.layout.color_spinner_layout)
+        adapter.setDropDownViewResource(R.layout.spinner_style)
+        binding.spinnerDiary.adapter = adapter
     }
-}
-}
-// 이미지 실제 경로 반환
-fun getRealPathFromURI(uri: Uri): String {
-val buildName = Build.MANUFACTURER
-if(buildName.equals("Xiaomi")){
-    return uri.path!!
-}
-var columnIndex = 0
-val proj = arrayOf(MediaStore.Images.Media.DATA)
-val cursor = requireActivity().contentResolver.query(uri, proj, null,null,null)
-if(cursor!!.moveToFirst()){
-    columnIndex = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA)
-}
-val result = cursor.getString(columnIndex)
-cursor.close()
-return result
-}
-// 갤러리를 부르는 메서드
-private fun selectGallery(){
-val writePermission = ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE)
-val readPermission = ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.READ_EXTERNAL_STORAGE)
+    private fun setupSpinnerHandler() {
+        binding.spinnerDiary.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
 
-// 권한 확인
-if(writePermission == PackageManager.PERMISSION_DENIED || readPermission == PackageManager.PERMISSION_DENIED){
-    // 권한 요청
-    requestPermissions(arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE), REQ_GALLERY)
-}else{
-    // 권한이 있는 경우 갤러리 실행
-    val intent = Intent(Intent.ACTION_PICK)
-    // intent의 data와 type을 동시에 설정하는 메서드
-    intent.setDataAndType(
-        MediaStore.Images.Media.EXTERNAL_CONTENT_URI,"image/*"
-    )
-    imageResult.launch(intent)
-}
-}
-override fun onDestroyView() {
-super.onDestroyView()
-mBinding = null
-}
+            }
+
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+
+            }
+        }
+    }
+    // 이미지 실제 경로 반환
+    fun getRealPathFromURI(uri: Uri): String {
+        val buildName = Build.MANUFACTURER
+        if(buildName.equals("Xiaomi")){
+            return uri.path!!
+        }
+        var columnIndex = 0
+        val proj = arrayOf(MediaStore.Images.Media.DATA)
+        val cursor = requireActivity().contentResolver.query(uri, proj, null,null,null)
+        if(cursor!!.moveToFirst()){
+            columnIndex = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA)
+        }
+        val result = cursor.getString(columnIndex)
+        cursor.close()
+        return result
+    }
+    // 갤러리를 부르는 메서드
+    private fun selectGallery(){
+        val writePermission = ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE)
+        val readPermission = ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.READ_EXTERNAL_STORAGE)
+        // 권한 확인
+        if(writePermission == PackageManager.PERMISSION_DENIED || readPermission == PackageManager.PERMISSION_DENIED){
+            // 권한 요청
+            requestPermissions(arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE), REQ_GALLERY)
+        }else{
+            // 권한이 있는 경우 갤러리 실행
+            val intent = Intent(Intent.ACTION_PICK)
+            // intent의 data와 type을 동시에 설정하는 메서드
+            intent.setDataAndType(
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI,"image/*"
+            )
+            imageResult.launch(intent)
+        }
+    }
+    override fun onDestroyView() {
+        super.onDestroyView()
+        mBinding = null
+    }
+    fun toast(message:String){
+        Toast.makeText(requireContext(), message, Toast.LENGTH_SHORT).show()
+    }
 }
