6e126cfef0f483d2ab6062f47f9f58ff26194cb3
jung eunjung <agjjung@avatye.com>
2023-01-21 20:48:13 +0900

    ➕ add : compose view base 추가 구현

--- app/src/main/java/com/shootit/greme/ui/view/ChallengeGuideActivity.kt
+++ app/src/main/java/com/shootit/greme/ui/view/ChallengeGuideActivity.kt
@@ -2,6 +2,9 @@ package com.shootit.greme.ui.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
 import androidx.core.view.ViewCompat
 import androidx.databinding.DataBindingUtil
 import com.shootit.greme.R
@@ -18,5 +21,13 @@ class ChallengeGuideActivity : AppCompatActivity() {
         binding
         ViewCompat.setTransitionName(binding.icon, "icon")
 
+        binding.composeView.setContent {
+            GuideTopActivity()
+        }
     }
+}
+
+@Composable
+fun GuideTopActivity() {
+    Text(text = "sample")
 }
