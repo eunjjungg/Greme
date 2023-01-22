c8664cebc3dd87d0d6cf026e2fc855c448585f90
jung eunjung <agjjung@avatye.com>
2023-01-22 15:43:15 +0900

    ✨ feature : Other User Profile api module 구현
    > Related to : #28

---
+++ app/src/main/java/com/shootit/greme/model/OtherUserProfileServerModel.kt
@@ -0,0 +1,14 @@
+package com.shootit.greme.model
+
+import com.google.gson.annotations.SerializedName
+
+data class OtherUserProfileModel(
+    @SerializedName("username")
+    val username: String,
+    @SerializedName("profileImg")
+    val profileImg: String,
+    @SerializedName("challengeSummary")
+    val challengeSummary: List<ChallengeDetail?>,
+
+)
+
